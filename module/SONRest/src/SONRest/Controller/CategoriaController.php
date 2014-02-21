b9db82cf78f6156e8638b6969c423895f965e4e0
vinnyfs89 <viniciusfesil@gmail.com>
2014-02-21 10:07:14 -0300

    [update]Finalização do módulo Rest.

--- module/SONRest/src/SONRest/Controller/CategoriaController.php
+++ module/SONRest/src/SONRest/Controller/CategoriaController.php
@@ -6,7 +6,7 @@ use Zend\Mvc\Controller\AbstractRestfulController;
 use Zend\View\Model\JsonModel;
 
 class CategoriaController extends AbstractRestfulController{
-    
+    #GET
     public function getList() {
         $em = $this->getServiceLocator()->get("Doctrine\ORM\EntityManager");
         $data = $em->getRepository("Application\Entity\Categoria")->findAll();
@@ -14,20 +14,46 @@ class CategoriaController extends AbstractRestfulController{
         return $data;
     }
     
+    #GET
     public function get($id) {
+        $em = $this->getServiceLocator()->get("Doctrine\ORM\EntityManager");
+        $data = $em->getRepository("Application\Entity\Categoria")->find($id);
         
+        return $data;
     }
     
-    public function create() {
-        
+    #POST
+    public function create($data) {
+        $serviceCategoria = $this->getServiceLocator()->get("Application\Service\Categoria");
+        $nome = $data['nome'];
+        $categoria = $serviceCategoria->insert($nome);
+        if($categoria) {
+            return $categoria;
+        }
+        return array('success' => false);
     }
     
-    public function update() {
+    #PUT - é um tipo de requisição HTTP, que geralmente não podem ser testadas via browser
+    public function update($id, $data) {
+        $serviceCategoria = $this->getServiceLocator()->get("Application\Service\Categoria");
+        $nome = $data['nome'];
         
+        $param['id'] = $id;
+        $param['nome'] = $data['nome'];
+        $categoria = $serviceCategoria->update($param);
+        if($categoria) {
+            return $categoria;
+        }
+        return array('success' => false);
     }
     
-    public function delete() {
-        
+    #DELETE
+    public function delete($id) {
+        $serviceCategoria = $this->getServiceLocator()->get("Application\Service\Categoria");
+        $result = $serviceCategoria->delete($id);
+        if($result) {
+            return $result;
+        }
     }
     
 }
