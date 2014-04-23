3f89a49b3a68ec248dc1cfb5c346a146a70c7d10
vinnyfs89 <viniciusfesil@gmail.com>
2014-04-23 04:44:05 +0000

    [backup] versionamento de backup

--- public/projetoangular/CategoriasCtrl.js
+++ public/projetoangular/CategoriasCtrl.js
@@ -2,14 +2,26 @@
  * Categorias Controller
  */
 categorias.controller('CategoriasCtrl', 
-    ["$scope", "CategoriasSrv", function($scope, CategoriasSrv) {
+    ["$scope", "CategoriasSrv", "$location",
+        function($scope, CategoriasSrv, $location) {
             $scope.nome = "Vinícius";
             
             // criando um método
             $scope.load = function() {
                 $scope.registros = CategoriasSrv.query();
             };
-            
             //executando
             $scope.load();
+            $scope.add = function(item) {
+                $scope.result = CategoriasSrv.save(
+                    {},
+                    item, 
+                    function(data, status, header, config) {
+                        $location.path('/categorias/');
+                    },
+                    function(data, status, header, config) {
+                        alert('Erro ao inserir registro: ' + data);
+                    }
+                );
+            }
     }]); 
